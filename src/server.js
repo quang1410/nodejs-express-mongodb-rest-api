26a1046c8cb574579e64a06f727d9330d48f6a2f
Quang Dang <quangdang@lecle.co.kr>
2023-09-12 22:44:26 +0700

    feat: :sparkles: add connect to mongodb and config env

--- src/server.js
+++ src/server.js
@@ -1,3 +1,4 @@
+/* eslint-disable no-console */
 /**
  * Updated by trungquandev.com's author on August 17 2023
  * YouTube: https://youtube.com/@trungquandev
@@ -5,28 +6,44 @@
  */
 
 import express from 'express'
-import { mapOrder } from '~/utils/sorts.js'
+import { CONNECT_DB } from '~/config/mongodb';
+import 'dotenv/config';
 
-const app = express()
 
-const hostname = 'localhost'
-const port = 8017
+const START_SERVER = () => {
+  const app = express()
+  const hostname = 'localhost'
+  const port = 8017
 
-app.get('/', (req, res) => {
-  // Test Absolute import mapOrder
-  console.log(mapOrder(
-    [ { id: 'id-1', name: 'One' },
-      { id: 'id-2', name: 'Two' },
-      { id: 'id-3', name: 'Three' },
-      { id: 'id-4', name: 'Four' },
-      { id: 'id-5', name: 'Five' } ],
-    ['id-5', 'id-4', 'id-2', 'id-3', 'id-1'],
-    'id'
-  ))
-  res.end('<h1>Hello World!</h1><hr>')
-})
+  app.get('/', (req, res) => {
+    res.end('<h1>Hello World!</h1><hr>')
+  })
 
-app.listen(port, hostname, () => {
-  // eslint-disable-next-line no-console
-  console.log(`Hello Trung Quan Dev, I am running at ${ hostname }:${ port }/`)
-})
+  app.listen(port, hostname, () => {
+    // eslint-disable-next-line no-console
+    console.log(`Hello Ngoc Quang Dev, I am running at ${hostname}:${port}`)
+  })
+};
+
+(async () => {
+  try {
+    console.log('1. Connecting to mongoDB Cloud Atlas');
+    await CONNECT_DB();
+
+    console.log('2. Connected to mongodb cloud');
+    START_SERVER();
+  } catch (error) {
+    console.error(error);
+    process.exit(0);
+  }
+})()
+
+//Cách viết khác:
+
+// CONNECT_DB()
+//   .then(() => console.log('Connected to mongodb cloud'))
+//   .then(() => START_SERVER())
+//   .catch((error) => {
+//     console.error(error);
+//     process.exit(0);
+//   })
